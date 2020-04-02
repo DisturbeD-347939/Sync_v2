ab5e7f8200ba5927fb63e1144866031f229aa061
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-04-02 02:46:57 +0100

    Moved

--- public/js/home.js
+++ public/js/home.js
@@ -88,12 +88,6 @@ $(document).ready(function()
         }
     })
 
-function getCookie(cname) 
-{
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) 
     {
         var c = ca[i];
         while (c.charAt(0) == ' ') 
@@ -101,17 +95,31 @@ function getCookie(cname)
           c = c.substring(1);
         }
         if (c.indexOf(name) == 0) 
+    function getCookie(cname) 
+    {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) 
         {
-          return c.substring(name.length, c.length);
+            var c = ca[i];
+            while (c.charAt(0) == ' ') 
+            {
+              c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) 
+            {
+              return c.substring(name.length, c.length);
+            }
         }
+        return "";
     }
-    return "";
-}
 
-function setCookie(cname, cvalue, exdays) 
-{
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
+    function setCookie(cname, cvalue, exdays) 
+    {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+})
